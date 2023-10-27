5817764dcf26307e4d7abe8ec4e17da542879d14
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2023-10-27 01:09:28 -0500

    added search bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-  import NavMenu from '@/components/NavMenu.vue'
+  import NavBar from './components/NavBar.vue'
+  import Main from './components/Main.vue'
 </script>
 
 <template>
-  <NavMenu />
-  <RouterView />
+  <NavBar />
+  <Main />
 </template>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import MainSearch from './MainSearch.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+        <MainSearch />
+        <h1>Hello There</h1>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+    .wrapper {
+        @apply flex flex-col items-center justify-center h-screen;
+    }
+
+</style>
