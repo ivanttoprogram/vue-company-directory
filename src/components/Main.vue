d32fae1424d2d6e05c16c23d22d0576747f14d47
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2023-10-29 19:20:28 -0500

    Final Staged Changes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,20 @@
 <script setup>
     import MainSearch from './MainSearch.vue'
     import MainCards from './MainCards.vue'
+    import MainPagination from './MainPagination.vue'
 </script>
 
 <template>
     <div class="wrapper">
         <MainSearch />
         <MainCards />
+        <MainPagination />
     </div>
 </template>
 
 <style scoped lang="postcss">
     .wrapper {
-        @apply fcontainer mx-auto flex flex-col gap-4
+        @apply container mx-auto flex flex-col gap-4
     }
 
 </style>
--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -29,12 +29,12 @@
   .pagination {
     @apply flex justify-center gap-4;
     .action {
-      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-800 hover:disabled:bg-slate-100;
     }
     .page {
       @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
       &.active {
-        @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
+        @apply bg-blue-900 text-slate-100 hover:bg-yellow-600;
       }
     }
   }
